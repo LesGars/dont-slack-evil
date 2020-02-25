61e2ac055449a633499c6aff38a5bd0c1e033edd
gjgd <gjgd@protonmail.com>
2020-02-25 12:17:33 +0700

    Add incoming webhook demo

---
+++ incoming-webhook/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// Response is of type APIGatewayProxyResponse since we're leveraging the
+// AWS Lambda Proxy Request functionality (default behavior)
+type Response events.APIGatewayProxyResponse
+
+// Handler function for the incoming webhook endpoint
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	webhookURL := os.Getenv("SLACK_INCOMING_WEBHOOK_URL")
+	fmt.Println(webhookURL)
+
+	reqBody, err := json.Marshal(map[string]string{
+		"text": "Hello world from the incoming-webhook lambda",
+	})
+	if err != nil {
+			fmt.Println(err.Error())
+	}
+	resp, err := http.Post(webhookURL,
+		"application/json", bytes.NewBuffer(reqBody))
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	return events.APIGatewayProxyResponse {
+		Body: string(body),
+		StatusCode: 200,
+		Headers: map[string]string{
+			"content-type": "text/plain",
+		},
+	}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
+}
