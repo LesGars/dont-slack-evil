873aaa595d5ea810432c63d57359d1fcaf4372eb
Cyril Duchon-Doris <cyril.duchon-doris@telecom-paristech.org>
2020-02-27 00:48:55 +0100

    Fix code to pass tests

--- apphome/apphome.go
+++ apphome/apphome.go
@@ -17,15 +17,18 @@ func UserHome(userId string) slack.Message {
 	headerSection := slack.NewSectionBlock(headerText, nil, slack.NewAccessory(headerButton))
 
 	// Hello
-	helloText := slack.NewTextBlockObject("mrkdwn", fmt.Sprintf(" :wave: Hello %s · find your DSE stats below", translateUserIdToUserName(userId)), false, false)
+	helloText := slack.NewTextBlockObject("plain_text", fmt.Sprintf(" :wave: Hello %s · find your DSE stats below", translateUserIdToUserName(userId)), true, false)
 	hellloSection := slack.NewSectionBlock(helloText, nil, nil)
 
 	// Build Message with blocks created above
-	return slack.NewBlockMessage(
+	message := slack.NewBlockMessage(
 		headerSection,
 		hellloSection,
 		divSection,
 	)
+	message.Msg.Type = "home"
+
+	return message
 }
 
 func translateUserIdToUserName(userId string) string {
