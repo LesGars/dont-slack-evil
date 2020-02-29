7cfb8e06ebd4c6b55c80e3f3a94e08fec44c8c51
Ismaïl MOURTADA <ismail.mourtada@gmail.com>
2020-03-01 00:53:47 +0100

    Temporarily comment code to handle the AppHomeOpened event

--- messages/main.go
+++ messages/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
-	"os"
 	"bytes"
 	"context"
 	"encoding/json"
-	"github.com/slack-go/slack"
-	"github.com/slack-go/slack/slackevents"
+	"os"
+
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
+
+	//"dont-slack-evil/apphome"
 )
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
@@ -22,38 +25,42 @@ var api = slack.New(slackOauthToken)
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(ctx context.Context, body []byte) (Response, error) {
-		buf := new(bytes.Buffer)
-		resp := Response{
-			IsBase64Encoded: false,
-			Headers: map[string]string{
-				"Content-Type":           "application/json",
-			},
-		}
-		eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: slackOauthToken}))
-		if e != nil {
+	buf := new(bytes.Buffer)
+	resp := Response{
+		IsBase64Encoded: false,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}
+	eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: slackOauthToken}))
+	if e != nil {
+		resp.StatusCode = 500
+	}
+
+	if eventsAPIEvent.Type == slackevents.Message {
+		var r *slackevents.ChallengeResponse
+		err := json.Unmarshal([]byte(body), &r)
+		if err != nil {
 			resp.StatusCode = 500
 		}
+		resp.Headers["Content-Type"] = "text"
+		buf.Write([]byte(r.Challenge))
+		resp.Body = buf.String()
+	}
+	if eventsAPIEvent.Type == slackevents.CallbackEvent {
+		innerEvent := eventsAPIEvent.InnerEvent
+		switch ev := innerEvent.Data.(type) {
+		case *slackevents.AppMentionEvent:
+			api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
+		case *slackevents.AppHomeOpenedEvent:
+			//TODO Use the slack.HomeTabViewRequest struct correctly, and find the hash
 
-		if eventsAPIEvent.Type == slackevents.Message {
-			var r *slackevents.ChallengeResponse
-			err := json.Unmarshal([]byte(body), &r)
-			if err != nil {
-				resp.StatusCode = 500
-			}
-			resp.Headers["Content-Type"] = "text"
-			buf.Write([]byte(r.Challenge))
-			resp.Body = buf.String()
+			//api.PublishView(ev.User, slack.HomeTabViewRequest{"home", apphome.UserHome("Cyril")}, "")
 		}
-		if eventsAPIEvent.Type == slackevents.CallbackEvent {
-			innerEvent := eventsAPIEvent.InnerEvent
-			switch ev := innerEvent.Data.(type) {
-			case *slackevents.AppMentionEvent:
-				api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
-			}
-		}
+	}
 
-		return resp, nil
-	
+	return resp, nil
+
 }
 
 func main() {
