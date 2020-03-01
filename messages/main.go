3e42165a74c69c658f4b70c92f5737c8978cfbe0
Ismaïl MOURTADA <ismail.mourtada@gmail.com>
2020-03-01 18:05:11 +0100

    Use APIGatewayProxyRequest to extract the request payload

--- messages/main.go
+++ messages/main.go
@@ -2,16 +2,15 @@ package main
 
 import (
 	"bytes"
-	"context"
 	"encoding/json"
 	"os"
+
+	"dont-slack-evil/apphome"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
-
-	//"dont-slack-evil/apphome"
 )
 
 // Response is of type APIGatewayProxyResponse since we're leveraging the
@@ -20,27 +19,32 @@ import (
 // https://serverless.com/framework/docs/providers/aws/events/apigateway/#lambda-proxy-integration
 type Response events.APIGatewayProxyResponse
 
+// Request is of type APIGatewayProxyRequest
+type Request events.APIGatewayProxyRequest
+
 var slackOauthToken = os.Getenv("SLACK_OAUTH_ACCESS_TOKEN")
 var slackSigningSecret = os.Getenv("SLACK_SIGNING_SECRET")
 var api = slack.New(slackOauthToken)
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
-func Handler(ctx context.Context, body []byte) (Response, error) {
+func Handler(request Request) (Response, error) {
 	buf := new(bytes.Buffer)
+	body := []byte(request.Body)
 	resp := Response{
 		IsBase64Encoded: false,
 		Headers: map[string]string{
 			"Content-Type": "application/json",
 		},
 	}
-	eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: slackSigningSecret}))
+	eventsAPIEvent, e := slackevents.ParseEvent(json.RawMessage(body),
+		slackevents.OptionVerifyToken(&slackevents.TokenComparator{VerificationToken: slackSigningSecret}))
 	if e != nil {
 		resp.StatusCode = 500
 	}
 
 	if eventsAPIEvent.Type == slackevents.Message {
 		var r *slackevents.ChallengeResponse
-		err := json.Unmarshal([]byte(body), &r)
+		err := json.Unmarshal(body, &r)
 		if err != nil {
 			resp.StatusCode = 500
 		}
@@ -54,9 +58,10 @@ func Handler(ctx context.Context, body []byte) (Response, error) {
 		case *slackevents.AppMentionEvent:
 			api.PostMessage(ev.Channel, slack.MsgOptionText("Yes, hello.", false))
 		case *slackevents.AppHomeOpenedEvent:
-			//TODO Use the slack.HomeTabViewRequest struct correctly, and find the hash
-
-			//api.PublishView(ev.User, slack.HomeTabViewRequest{"home", apphome.UserHome("Cyril")}, "")
+			api.PublishView(
+				ev.User,
+				slack.HomeTabViewRequest{"home", apphome.UserHome("Cyril").Blocks, "", "", ""},
+				ev.View.Hash)
 		}
 	}
 
