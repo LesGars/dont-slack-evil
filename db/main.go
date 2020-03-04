fdfdc27fe8f66de9090476be37b3794711888e2d
gjgd <gjgd@protonmail.com>
2020-03-05 04:07:18 +0900

    Add store func in db service

--- db/main.go
+++ db/main.go
@@ -1,15 +1,14 @@
 package db
 
 import (
-	"strings"
 	"log"
+	"strings"
 
-	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
-	// "github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
-
 
 // CreateDBIfNotCreated creates DynamoDB table if it doesn't exist
 func CreateDBIfNotCreated(tableName string) bool {
@@ -43,6 +42,32 @@ func CreateDBIfNotCreated(tableName string) bool {
 		}
 	} else {
 		log.Println("Created the table", tableName)
+	}
+	return true
+}
+
+// Store stores an item in the database
+func Store(tableName string, item map[string]string) bool {
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+	svc := dynamodb.New(sess)
+
+	av, err := dynamodbattribute.MarshalMap(item)
+	if err != nil {
+		log.Println("Got error marshalling new item:")
+		log.Println(err.Error())
+		return false
+	}
+	putItemInput := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+	_, err = svc.PutItem(putItemInput)
+	if err != nil {
+		log.Println("Got error calling PutItem:")
+		log.Println(err.Error())
+		return false
 	}
 	return true
 }
