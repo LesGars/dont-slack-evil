5607f8068911b420fe147b813c472a7cfb622ee9
gjgd <gjgd@protonmail.com>
2020-03-08 04:11:36 +0900

    Get access token

--- lambda/redirectUrl/redirectUrl.go
+++ lambda/redirectUrl/redirectUrl.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"log"
+	"os"
+	"net/http"
+	"net/url"
+	"strings"
+	"io/ioutil"
+	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	// "dont-slack-evil/messages/service"
-	// "github.com/fatih/structs"
-
 )
 
+// OauthAccessResponse is the type of the Oauth Access response
+type OauthAccessResponse struct {
+	Ok bool `json:"ok"`
+	AppID string `json:"app_id"`
+	Scope string `json:"scope"`
+	TokenType string `json:"token_type"`
+	AccessToken string `json:"access_token"`
+	BotUserID string `json:"bot_user_id"`
+	Team struct {
+		ID string `json:"id"`
+		Name string `json:"name"`
+	} `json:"team"`
+	Entreprise string `json:"entreprise"`
+}
 // Response is of type APIGatewayProxyResponse
 type Response events.APIGatewayProxyResponse
 
@@ -18,21 +35,43 @@ type Request events.APIGatewayProxyRequest
 
 // Handler is our lambda handler invoked by the `lambda.Start` function call
 func Handler(request Request) (Response, error) {
-	body := []byte(request.Body)
 	query := request.QueryStringParameters
-	log.Printf("Receiving request body %s", body)
-	log.Printf("Receiving request query string %s", query)
-	redirectUrl := "http://lol.com"
-	resp := Response{
-		StatusCode: 200,
-		Body: redirectUrl,
-		// StatusCode: 302,
-		// Headers: map[string]string{
-		// 	"Location": redirectUrl,
-		// },
-		// Body: "",
+	code := query["code"]
+	clientID := os.Getenv("CLIENT_ID")
+	clientSecret := os.Getenv("CLIENT_SECRET")
+	oauthURL := "https://slack.com/api/oauth.v2.access"
+	form := url.Values{}
+	form.Add("code", code)
+	form.Add("client_id", clientID)
+	form.Add("client_secret", clientSecret)
+	resp, err := http.Post(
+		oauthURL,
+		"application/x-www-form-urlencoded",
+		strings.NewReader(form.Encode()),
+	)
+	if err != nil {
+		log.Println(err)
 	}
-	return resp, nil
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	log.Printf("%s", body)
+	var oauthAccessResponse OauthAccessResponse;
+	unMarshallErr := json.Unmarshal(body, &oauthAccessResponse)
+	if unMarshallErr != nil {
+		log.Println(unMarshallErr)
+	}
+	log.Println(oauthAccessResponse)
+	botAccessToken := oauthAccessResponse.AccessToken
+	log.Println(botAccessToken)
+	redirectURL := "https://app.slack.com/client/" + oauthAccessResponse.Team.ID
+	response := Response{
+		StatusCode: 302,
+		Headers: map[string]string{
+			"Location": redirectURL,
+		},
+		Body: "",
+	}
+	return response, nil
 }
 
 func main() {
