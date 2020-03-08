d1bfd321c70f54e974d9b1401c6348be8238f18c
Cyril Duchon-Doris <cyril@myjobglasses.com>
2020-03-08 16:21:16 +0100

    Remove unused home sections, adjust tests to new test data

--- apphome/stats_test.go
+++ apphome/stats_test.go
@@ -9,8 +9,8 @@ import (
 func TestHomeStatsForUser(t *testing.T) {
 	// Stats correspond to arbitrary messages that were seeded in the test DB
 	// Using https://snippets.cacher.io/snippet/3ae7dcb2e44370bf4dfc
-	expectedObject := DSEHomeStats{50, 27, 0.54, 0, 0, 0}
-	actual := HomeStatsForUser("Alissa Kutch")
+	expectedObject := DSEHomeStats{50, 29, 0.58, 26, 14, 0.5384615384615384}
+	actual := HomeStatsForUser("Mr. Maggie Feest")
 
 	if diff := deep.Equal(expectedObject, actual); diff != nil {
 		t.Error(diff)
