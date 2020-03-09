56eda0c7e467929101a2905501dc8d59d958043e
Cyril Duchon-Doris <cyril@myjobglasses.com>
2020-03-09 01:27:45 +0100

    Send a proper ack response marshalled

--- interactive/slack_handler.go
+++ interactive/slack_handler.go
@@ -5,6 +5,7 @@ import (
 	"log"
 
 	"github.com/slack-go/slack"
+	"github.com/slack-go/slack/slackevents"
 )
 
 func SlackHandler(payload string, response Response) error {
@@ -18,15 +19,23 @@ func SlackHandler(payload string, response Response) error {
 	// See https://github.com/slack-go/slack/blob/510942f19cfde364380379e47f5b4f780b5fb477/interactions.go
 	switch interaction.Type {
 	case slack.InteractionTypeMessageAction:
-		acknowledge(response)
-		return nil
+		return acknowledge(response)
 	}
 
 	return nil
 }
 
-func acknowledge(response Response) {
+func acknowledge(response Response) error {
+	ack := slackevents.MessageActionResponse{
+		ResponseType:    slack.ResponseTypeEphemeral,
+		ReplaceOriginal: false,
+		Text:            "",
+	}
+	ackMarshalled, err := json.Marshal(ack)
+	if err != nil {
+		return err
+	}
 	response.StatusCode = 200
-	response.Body = "ok"
-	response.Headers["Content-Type"] = "text"
+	response.Body = string(ackMarshalled)
+	return nil
 }
