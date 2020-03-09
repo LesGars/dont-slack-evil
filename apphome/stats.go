bc476438bad9df1cd0b24c11310c02465ca4d75f
Ismaïl MOURTADA <ismail.mourtada@gmail.com>
2020-03-09 18:26:45 +0100

    Modify getAll process

--- apphome/stats.go
+++ apphome/stats.go
@@ -8,7 +8,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
-	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/jinzhu/now"
 )
 
--- notifications/service.go
+++ notifications/service.go
@@ -3,35 +3,40 @@ package notifications
 import (
 	"dont-slack-evil/apphome"
 	"log"
-	"os"
-
+	dsedb "dont-slack-evil/db"
 	"github.com/slack-go/slack"
 )
-
-var slackBotUserOauthToken = os.Getenv("SLACK_BOT_USER_OAUTH_ACCESS_TOKEN")
-var slackBotUserApiClient = slack.New(slackBotUserOauthToken)
 
 // SendNotification loops through all the users and sends a message to those who sent too many
 // messages of bad quality over the last quarter
 func SendNotifications() (int, error) {
 	notificationsSent := 0
-	users, err := slackBotUserApiClient.GetUsers()
-	if err != nil {
-		return 0, err
+	teams, teamsErr := dsedb.GetTeams()
+	if teamsErr != nil {
+		return 0, teamsErr
 	}
-	for _, user := range users {
-		userId := user.ID
-		tooManyBadQualityMessagesLastQuarter := apphome.HasTooManyBadQualityMessagesLastQuarter(userId)
-		if tooManyBadQualityMessagesLastQuarter {
-			conversationParameters := slack.OpenConversationParameters{
-				Users: []string{userId},
-			}
-			channel, _, _, conversationErr := slackBotUserApiClient.OpenConversation(&conversationParameters)
-			if conversationErr != nil {
-				log.Printf("Could not open conversation for user %v: %v", userId, conversationErr)
-			} else {
-				slackBotUserApiClient.PostMessage(channel.ID, slack.MsgOptionText("Too many bad quality messages", false))
-				notificationsSent++
+
+	for _, team := range teams {
+		slackBotUserApiClient := slack.New(team.SlackBotUserToken)
+		users, err := slackBotUserApiClient.GetUsers()
+		if err != nil {
+			log.Printf("Could not instantiate bot client for team %v", team.SlackTeamId)
+			continue
+		}
+		for _, user := range users {
+			userId := user.ID
+			tooManyBadQualityMessagesLastQuarter := apphome.HasTooManyBadQualityMessagesLastQuarter(userId)
+			if tooManyBadQualityMessagesLastQuarter {
+				conversationParameters := slack.OpenConversationParameters{
+					Users: []string{userId},
+				}
+				channel, _, _, conversationErr := slackBotUserApiClient.OpenConversation(&conversationParameters)
+				if conversationErr != nil {
+					log.Printf("Could not open conversation for user %v: %v", userId, conversationErr)
+				} else {
+					slackBotUserApiClient.PostMessage(channel.ID, slack.MsgOptionText("Too many bad quality messages", false))
+					notificationsSent++
+				}
 			}
 		}
 	}
