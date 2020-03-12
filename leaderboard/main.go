4f797be87a679f0548ba13bf169419c5ee59bfba
gjgd <gjgd@protonmail.com>
2020-03-13 02:59:02 +0900

    WIP: Display IDs of Team members that are not bots

--- leaderboard/main.go
+++ leaderboard/main.go
@@ -3,21 +3,45 @@ package leaderboard
 import (
 	"log"
 
+	"dont-slack-evil/apphome"
 	dsedb "dont-slack-evil/db"
+	"github.com/slack-go/slack"
 )
 
 // SendLeaderboardNotification sends the leaderboard notification
 func SendLeaderboardNotification() (int, error) {
-	log.Println("Hi Im here")
-	log.Println("mdr")
 	notificationsSent := 0
-	log.Println("LOL")
-	log.Println(dsedb.GetTeams)
 	teams, teamsErr := dsedb.GetTeams()
 	if teamsErr != nil {
 		log.Println(teamsErr)
 		return 0, teamsErr
 	}
-	log.Println(teams)
+	for _, team := range teams {
+		// FIXME: remove
+		if (team.SlackTeamId != "TU7KB9FB9") {
+			continue
+		}
+		slackBotUserApiClient := slack.New(team.SlackBotUserToken)
+		users, err := slackBotUserApiClient.GetUsers()
+		if err != nil {
+			log.Printf("Could not instantiate bot client for team %v", team.SlackTeamId)
+			continue
+		}
+		log.Println(team.SlackTeamId)
+		type UserScore struct {
+			userID string
+			userScore float64
+		}
+		var userScores []UserScore;
+		for _, user := range users {
+			// This is the best way I found to distinguish bots from real users
+			// Note that user.IsBot doesn't work because it's false even for bot users...
+			if (len(user.Profile.BotID) == 0) {
+				log.Println(user.RealName)
+				log.Println(user.ID)
+			}
+		}
+	}
+
 	return notificationsSent, nil
 }
